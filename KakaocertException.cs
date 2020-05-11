deca3d8713f30dfd2fad62d8a4238cc493ad965f
JohnJeong123 <frenchofkiss@gmail.com>
2020-05-11 15:45:30 +0900

    added Exception

---
+++ KakaocertException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kakaocert
+{
+    class KakaocertException : Exception
+    {
+          public KakaocertException()
+            : base()
+        {
+        }
+          public KakaocertException(long code, String Message)
+            : base(Message)
+        {
+            this._code = code;
+        }
+
+        private long _code;
+
+        public long code
+        {
+            get { return _code; }
+        }
+    }
+}
